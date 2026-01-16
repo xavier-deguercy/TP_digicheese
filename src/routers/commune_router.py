6fc8d8efd33a195caf2d187fead13ff8ccaf1e0c
Hiennguyenalice <nguyenthuhien1088@gmail.com>
2026-01-16 14:01:52 +0100

    modifier class commune

--- src/routers/commune_router.py
+++ src/routers/commune_router.py
@@ -2,4 +2,4 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 # create a router for communes-related endpoints
-router = APIRouter(prefix="/communes", tags=["communes"])
+router = APIRouter(prefix="/commune", tags=["commune"])
