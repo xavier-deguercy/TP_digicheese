931bce1e34bb5aedc3c4339f3da31884eb21a017
imen <khammassiimen4@gmail.com>
2026-01-20 09:47:18 +0100

    Models, Repositories, Routes, services -- Poids & Poids-Vignettes

--- src/models/poids.py
+++ src/models/poids.py
@@ -1,12 +1,12 @@
-from sqlalchemy import column, Integer, String, DECIMAL
+from sqlalchemy import Column, Integer, String, DECIMAL
 from .base import Base
 
 class Poids(Base) :
     """Table représentant les poids et timbres associés aux commandes."""
     __tablename__ = "t_poids"
 
-    id = column(Integer, default=None, primary_key=True)
-    valmin = column(DECIMAL, default=DECIMAL("0"), nullable=True)
-    valtimbre = column(DECIMAL, default=DECIMAL("0"), nullable=True)
+    id = Column(Integer, default=None, primary_key=True)
+    valmin = Column(DECIMAL, default=DECIMAL("0"), nullable=True)
+    valtimbre = Column(DECIMAL, default=DECIMAL("0"), nullable=True)
 
 
--- src/models/poidsv.py
+++ src/models/poidsv.py
@@ -1,11 +1,11 @@
-from sqlalchemy import column, Integer, DECIMAL
+from sqlalchemy import Column, Integer, DECIMAL
 from .base import Base
 
 class Poidsv(Base) :
     """Table qui représente les vignettes ( timbre ) avec leurs prix pour un certain poids."""
     __tablename__ = "t_poidsv"
 
-    id = column(Integer, default=None, primary_key=True)
-    valmin = column(DECIMAL, default=DECIMAL("0"), nullable=True)
-    valtimbre = column(DECIMAL, default=DECIMAL("0"), nullable=True)
+    id = Column(Integer, default=None, primary_key=True)
+    valmin = Column(DECIMAL, default=DECIMAL("0"), nullable=True)
+    valtimbre = Column(DECIMAL, default=DECIMAL("0"), nullable=True)
    
