6164062abfb5fca268e08ce85a5bbcb9b9febb01
Stanislas Delannoy <stanislas.delannoy@outlook.fr>
2026-01-20 15:28:20 +0100

    Suppression de la relation Objet Cond

--- src/models/conditionnement.py
+++ src/models/conditionnement.py
@@ -1,6 +1,6 @@
 # src/models/conditionnement.py
 
-from sqlalchemy import Column, Integer, String, Numeric     
+from sqlalchemy import Column, Integer, String, Numeric
 from sqlalchemy.orm import relationship
 from .base import Base
 
@@ -15,5 +15,5 @@ class Conditionnement(Base):
     prix_condit = Column(Numeric(10, 4), nullable=False, default=0)             # prix unitaire conditionnement
     ordre_imp = Column(Integer, nullable=True)                                  # ordre d'impression conditionnement
 #relation
-    objet_cond = relationship("ObjetCond", back_populates="condit")             # relation avec la table des objets
+
 #pas de methode ici, ce sera dans les routes
